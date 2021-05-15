719fb9685f88f9b8a6aba93e4573831f418105dd
Jordan <CircuitLord@users.noreply.github.com>
2021-05-15 17:01:07 -0500

    initial

---
+++ PhysicsBooster/PhysicsBoosterMod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace PhysicsBooster {
+
+	public class PhysicsBoosterMod : MelonMod {
+
+		private const string PREF_CATAGORY = "PhysicsBooster";
+
+		private const string PREF_PHYSICS_FPS = "PhysicsFPS";
+
+		private int targetPhysicsFPS = 144;
+		private float targetPhysicsTick = 0.006944f;
+
+		public override void OnApplicationStart() {
+			base.OnApplicationStart();
+
+			MelonPreferences.Load();
+			
+			MelonPreferences.CreateCategory(PREF_CATAGORY, "Settings");
+
+			MelonPreferences.CreateEntry(PREF_CATAGORY, PREF_PHYSICS_FPS, 144, "Physics FPS");
+
+			targetPhysicsFPS = MelonPreferences.GetEntryValue<int>(PREF_CATAGORY, PREF_PHYSICS_FPS);
+			targetPhysicsTick = 1f / targetPhysicsFPS;
+			
+			MelonPreferences.Save();
+
+			MelonLogger.Msg("Physics booster init... Target framerate is " + targetPhysicsFPS + ". Target physics tick is " + targetPhysicsTick.ToString("F4"));
+
+			Time.fixedDeltaTime = targetPhysicsTick;
+			
+		}
+		
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+			base.OnSceneWasLoaded(buildIndex, sceneName);
+			
+			Time.fixedDeltaTime = targetPhysicsTick;
+		}
+
+	}
+
+}
---
+++ PhysicsBooster/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using PhysicsBooster;
+
+[assembly: MelonInfo(typeof(PhysicsBoosterMod), "Physics Booster", "1.0", "CircuitLord")]
+[assembly: MelonGame(null, null)]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PhysicsBooster")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhysicsBooster")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32584b7d-e857-4545-bbc5-a697f23e1505")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
